1e4e25cdaaa52d7c3514e80261a6d38532c8f3c0
[Aqsin_Heyderli] <[heyderliaqsin94@gmail.com]>
2022-08-01 17:56:39 +0400

    Add project files.

---
+++ Jwt_Web_Api/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Jwt_Web_Api
+{
+    public class User
+    {
+        public string Username { get; set; }=string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
