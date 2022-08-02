329ae5ae5bc4eb2ac6b96e4ef3636c4605b28b48
[Aqsin_Heyderli] <[heyderliaqsin94@gmail.com]>
2022-08-02 15:08:13 +0400

    <Refresh-token>

---
+++ Jwt_Web_Api/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Jwt_Web_Api
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Expires { get; set; }
+    }
+}
